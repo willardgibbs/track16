31ad66dcd6c80c49125b7500e7a7294a8742470a
willardgibbs <kzavertkin@mail.ru>
2017-06-25 20:26:54 +0300

    realize main in Parse

--- src/main/java/track/lessons/lesson2/Parser.java
+++ src/main/java/track/lessons/lesson2/Parser.java
@@ -2,6 +2,8 @@ package track.lessons.lesson2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,11 +19,16 @@ public class Parser {
         String path = "path/to/file";
         BufferedReader reader = new BufferedReader(new FileReader(path));
         // reader умеет читать по строкам с помощью метода readLine()
-
+        StringBuilder lines = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {//как тут остановиться?
+            lines.append(line);
+        }
         // Создайте объект Parser
+        Parser pars = new Parser();
 
         // Получите объект Document, реализовав метод parse()
-
+        pars.parse(lines.toString());
 
     }
 }
